10bb26efff3ec58bd3b42a3c53fa85e59601ef73
bfturner <brendanfturner@gmail.com>
2014-12-04 09:18:58 -0600

    Initial commit

---
+++ server/subscriptions.js
@@ -0,0 +1,39 @@
+// Chatrooms publishing and permissions
+Meteor.publish('allChatrooms', function () {
+	return Chatrooms.find();
+});
+
+Chatrooms.allow({
+	'insert' : function () {
+		return true;
+	}
+});
+
+// Messages publishing and persmissions
+Meteor.publish('chatMessages', function(roomID) {
+	return Messages.find({ chatroom : roomID});
+});
+
+Messages.allow({
+	'insert' : function () {
+		return true;
+	}
+});
+
+// ChatroomUsers publishing and permissions
+Meteor.publish('chatUsers', function(roomID) {
+	return ChatroomUsers.find({ chatroom : roomID});
+});
+
+ChatroomUsers.allow({
+	'insert' : function() {
+		return true;
+	},
+	'update' : function() {
+		return true;
+	},
+	'remove' : function() {
+		return true;
+	}
+});
+

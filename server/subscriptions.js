a1d34d930f7d3d6c2f0bb0e12adef3ad7742b286
bfturner <brendanfturner@gmail.com>
2014-12-07 10:49:35 -0600

    Removed unnecessary collections
    Chatrooms is no longer needed since this will only cover one chatroom.
    
    ChatroomUsers is no longer needed because we will be covering online
    statuses with mizzao:user-status & Meteor.users.

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -9,21 +9,3 @@ Messages.allow({
 		return true;
 	}
 });
-
-// ChatroomUsers publishing and permissions
-Meteor.publish('chatUsers', function(roomID) {
-	return ChatroomUsers.find({ chatroom : roomID});
-});
-
-ChatroomUsers.allow({
-	'insert' : function() {
-		return true;
-	},
-	'update' : function() {
-		return true;
-	},
-	'remove' : function() {
-		return true;
-	}
-});
-

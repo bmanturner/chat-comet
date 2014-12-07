c1d58b8d53f4604b8526fb88c9c0d3d99722888a
bfturner <brendanfturner@gmail.com>
2014-12-07 11:19:50 -0600

    Implemented user-status package
    Online users can now be subscribed to and are displayed on the chatroom.

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -1,4 +1,10 @@
-// Messages publishing and persmissions
+// Online users
+Meteor.publish('onlineUsers', function () {
+	return Meteor.users.find({ "status.online": true });
+});
+
+
+// Messages publishing and permissions
 Meteor.publish('chatMessages', function() {
 	var timestamp = new Date();
 	return Messages.find({ creation_date: {$gte: timestamp} });

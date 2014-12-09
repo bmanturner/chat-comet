e603d379c36efcfe8610a9180bbe3703a327a1b7
bfturner <brendanfturner@gmail.com>
2014-12-09 11:57:26 -0600

    Log and display user events
    Messages are displayed when users login and logout.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,25 @@ Meteor.startup(function () {
 	Messages.remove({});
 });
 
+Meteor.users.find({ "status.online": true }).observe({
+  added: function(id) {
+    if(id) {
+		var username = id.username;
+		var message = "entered the chatroom";
+		var messageType = 'login';
+		Meteor.call('logEvent', username, message, messageType);
+	}
+  },
+  removed: function(id) {
+    if(id) {
+		var username = id.username;
+		var message = "left the chatroom";
+		var messageType = 'logout';
+		Meteor.call('logEvent', username, message, messageType);
+	}
+  }
+});
+
 Meteor.methods({
 	addMessage: function (username, usermessage) {
 		check(username, String);
@@ -10,7 +29,20 @@ Meteor.methods({
 			userID: this.userId,
 			user: username,
 			message: usermessage,
+			msgEvent: 'message',
 			creation_date: new Date()
 		});
+	},
+	logEvent: function (username, logmessage, type) {
+		check(username, String);
+		check(logmessage, String);
+		Messages.insert({
+			userID: this.userId,
+			user: username,
+			message: logmessage,
+			msgEvent: type,
+			creation_date: new Date()
+		});
+		console.log('done');
 	}
 });

a0c6dc096ff7c43450cb6f193c45a4297fcddb24
bfturner <brendanfturner@gmail.com>
2014-12-09 14:20:04 -0600

    Bug fix
    Online user observe must be declared in startup

--- server/server.js
+++ server/server.js
@@ -1,29 +1,31 @@
-// Clears message colleciton on start-up
 Meteor.startup(function () {
+	// Clears message colleciton on start-up
 	Messages.remove({});
+
+	// Monitors users online status and keeps log of activity
+	Meteor.users.find({ "status.online": true }).observe({
+		// User comes online
+	    added: function(id) {
+	    	if(id) {
+				var username = id.username;
+				var message = "entered the chatroom";
+				var messageType = 'login';
+				Meteor.call('logEvent', username, message, messageType);
+			}
+	  	},
+	  	// User goes offline
+	  	removed: function(id) {
+	    	if(id) {
+				var username = id.username;
+				var message = "left the chatroom";
+				var messageType = 'logout';
+				Meteor.call('logEvent', username, message, messageType);
+			}
+	  	}
+	});
 });
 
-// Monitors users online status and keeps log of activity
-Meteor.users.find({ "status.online": true }).observe({
-	// User comes online
-    added: function(id) {
-    	if(id) {
-			var username = id.username;
-			var message = "entered the chatroom";
-			var messageType = 'login';
-			Meteor.call('logEvent', username, message, messageType);
-		}
-  	},
-  	// User goes offline
-  	removed: function(id) {
-    	if(id) {
-			var username = id.username;
-			var message = "left the chatroom";
-			var messageType = 'logout';
-			Meteor.call('logEvent', username, message, messageType);
-		}
-  	}
-});
+
 
 // Meteor methods to add messages or events
 Meteor.methods({
